53e4d051188c6c543b6aeae0b672adcf8dbed5a9
Mohd Ruman <mohd.ruman.82@gmail.com>
2025-07-31 05:06:34 +0530

    Base animation successful.

--- src/app/page.js
+++ src/app/page.js
@@ -1,103 +1,127 @@
+'use client';
 import Image from "next/image";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
+import { useGSAP } from "@gsap/react";
+
+gsap.registerPlugin(ScrollTrigger);
 
 export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+  const images = [
+    {
+      title: 'Capsule',
+      image: '/cap1.png'
+    },
+    {
+      title: 'Cuboid',
+      image: '/cap2.png'
+    },
+    {
+      title: 'Desert Capsule',
+      image: '/cap3.png'
+    },
+  ]
+
+  useGSAP(() => {
+    const cards = gsap.utils.toArray(".card");
+
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: ".cards",
+        start: "center center",
+        end: "+=3000",
+        scrub: 2,
+        pin: true,
+      },
+    });
+
+    // STEP 1: First card expands from scale 0.5 to 1
+    tl.fromTo(cards[0],
+      {
+        scale: 0.4,
+        opacity: 0.8,
+        borderRadius: '300px'
+      },
+      {
+        scale: 1,
+        y: 0,
+        opacity: 1,
+        duration: 1.5,
+        borderRadius: '44px'
+      },
+    );
+
+    tl.from(cards[0].querySelector('h1'), {
+      y: 50,
+      opacity: 0, 
+      duration: 2
+    })
+    
+
+    cards.forEach((card, index) => {
+
+      //Exclude the first card
+      if (index === 0) return;
+
+      tl.from(card, {
+        y: "100vh",
+        duration: 1,
+        borderRadius: '44px'
+      }); 
+
+      // Animate previous card (if exists) out at the same time
+      if (index > 0) {
+        const prevCard = cards[index - 1];
+        tl.to(prevCard, {
+          opacity: 0,
+          scale: 0.9,
+          duration: 1
+        }, '<'); // '<' = start at same time as previous .from
+      }
+
+      // Fade in title for cards
+      tl.from(card.querySelector('h1'), {
+        y: 50,
+        opacity: 0, 
+        duration: 1
+      })
+    
+
+      
+    });
+  }, []);
+
+  return (
+    <>
+      <section className="w-screen h-dvh flex items-center justify-center">
+        <h1>We don't only create Architecture.</h1>
+      </section>
+
+      <section className="cards relative h-screen w-full">
+        {
+          images.map((image, index) => (
+            <div
+              className="card absolute top-0 left-0 w-full h-full flex items-center justify-center overflow-hidden rounded-[44px]"
+              key={index}
+            >
+              <img
+                src={image.image}
+                className="w-full h-full object-cover rounded-[inherit]"
+        
+                alt={`Card ${index}`}
+              />
+              <h1
+                className="absolute"
+              >{image.title}</h1>
+            </div>
+          ))
+        }
+      </section>
+
+      <section className="w-screen h-dvh flex items-center justify-center">
+        <h1>We define the way to architect.</h1>
+      </section>
+    </>
   );
 }
